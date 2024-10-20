b86c9fd0099ad1d24217db2bd0087157688e4fe8
Simon Chiu <simon@furvur.com>
2024-10-20 16:45:48 -0400

    Ensure permissions

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,5 +3,9 @@ function update() {
   # Upgrade the Broadcast scripts
   cd /opt/broadcast
   git pull
+
+  # Change ownership of the Broadcast directory to the broadcast user
+  sudo chown -R broadcast:broadcast /opt/broadcast
+
   echo -e "\e[32mBroadcast scripts upgraded successfully!\e[0m"
 }
